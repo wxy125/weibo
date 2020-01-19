fcff9e95b7aa74e611b2c9ead131e07310ada447
wxy <WXY@example.com>
2020-01-19 01:45:26 +0000

    用户注册表单

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,7 +5,6 @@ namespace App\Models;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use think\Controller;
 
 class User extends Authenticatable
 {
