a9cd99c5f5342cfc34e73e4f755c231540f145c1
wxy <WXY@example.com>
2020-01-19 01:39:59 +0000

    用户显示页面

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,10 +1,11 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use think\Controller;
 
 class User extends Authenticatable
 {
@@ -37,4 +38,6 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+
 }
