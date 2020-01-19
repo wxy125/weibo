dfa09a382cf1e26599723330fe270c6f3944f998
wxy <WXY@example.com>
2020-01-19 08:35:13 +0000

    Updated database configuration

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -10,9 +10,9 @@
 
     <div class="container">
       <div class="offset-md-1 col-md-10">
-          @include('shared._messages')
-          @yield('content')
-          @include('layouts._footer')
+        @include('shared._messages')
+        @yield('content')
+        @include('layouts._footer')
       </div>
     </div>
   </body>
